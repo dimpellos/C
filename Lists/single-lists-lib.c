2ca635901e3de195907cf65311b155f08b473c63
dimitrisbellos <dimitrisbellos@iti.gr>
2026-06-24 15:32:36 +0300

    include implentations to lists lib

--- Lists/single-lists-lib.c
+++ Lists/single-lists-lib.c
@@ -1,9 +1,44 @@
 node *rotateSingleList(node *head, int rotations) {
 
 }
-node *mergeSingleSortedLists(node *list1, node *list2)
+node *mergeSingleSortedLists(node *list1, node *list2) {
+    
+}
 node *mergeTwoSortedLists(node *list1, node *list2) {
+    if (list1 == NULL)
+        return list2;
+    if (list2 == NULL)
+        return list1;
 
+    struct ListNode *newList = NULL, *curr = NULL;
+    // new Root
+    if (list1->val < list2->val) {
+        newList = curr = list1;
+        list1 = list1->next;
+    } else {
+        newList = curr = list2;
+        list2 = list2->next;
+    }
+
+    while (list1 != NULL || list2 != NULL) {
+        if (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;                
+            }
+        } else if (list1 == NULL) {
+            curr->next = list2;
+            break;
+        } else if (list2 == NULL) {
+            curr->next = list1;
+            break;
+        }
+        curr = curr->next;
+    }
+    return newList;
 }
 node *rotateList(node *head, int rotations) {
     struct ListNode *cur = head;
@@ -52,6 +87,7 @@ node *reverseList(node *head) {
     cur->next = last;
     return cur;
 }
+
 int countNodes(node *head) {
     int n = 0;
     while (head != NULL) {
@@ -60,6 +96,7 @@ int countNodes(node *head) {
     }
     return n;
 }
+
 node *sortList(node *head, bool order = true) {
     if (!head || !head->next) return head;
     struct ListNode *newHead = head, *prev = head->next;
@@ -93,19 +130,52 @@ node *sortList(node *head, bool order = true) {
     }
     return newHead;
 }
+
 node *initList(int val) {
 
 }
 void addNode(node *head) {
 
 }
+
 void deleteNode(node *head, int val) {
 
 }
+
 void destroyList(node *head);
 void swapNodes(node *head, int val1, int val2);
-node *removeDuplicatesFromSortedList(node *head);
+node *removeDuplicatesFromSortedList(node *head) {
+        if (head == NULL || head->next == NULL)
+        return head;
+    
+    while (head->next != NULL && head->next->val == head->val) {
+        struct ListNode *cur2 = head->next;
+        head->next = cur2->next;
+        free(cur2);    
+    }
+    if (head->next == NULL) return head;
+
+    struct ListNode *cur = head->next->next, *prev = head->next;
+    while (cur != NULL) {
+        if (prev->val == cur->val) {
+            prev->next = cur->next;
+            free(cur);     
+        } else {
+            prev = cur;
+        }
+        cur = prev->next;
+    }
+    return head;
+}
 node *removeAllDuplicatesFromSortedList(node *head);
 node *convertSortedListToBinaryTree(node *head);
 // RETURN MIDDLE NODE OF THE LIST
-node *getMiddleNode(node *head);
+node *getMiddleNode(node *head) {
+    if (!head || !head->next) return head;
+    struct ListNode *slow = head->next, *fast = head->next->next;
+    while (fast != NULL && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
